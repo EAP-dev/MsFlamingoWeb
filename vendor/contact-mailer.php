8244e6142db0209d111cc85762920cbc94fb36f2
Enrico Petrarolo <eno@trancelantic.com>
2024-09-26 15:09:15 +0200

    updated index topBar icon and contact-mailer.php port

--- vendor/contact-mailer.php
+++ vendor/contact-mailer.php
@@ -275,7 +275,7 @@ if($_POST)
     $mail->SMTPAuth   = true;                                    //ENABLE SMTP AUTHENTICATION
     $mail->Username   = 'enrico.petrarolo@gmail.com';                //SMTP username
     $mail->Password   = 'qhtjikyvpapxshxu';                   //SMTP password
-    $mail->SMTPSecure = 'tls';          //ENABLE TLS ENCRYPTION; `PHPMAILER::ENCRYPTION_SMTPS` ALSO ACCEPTED
+    $mail->SMTPSecure = 'ssl';          //ENABLE TLS ENCRYPTION; `PHPMAILER::ENCRYPTION_SMTPS` ALSO ACCEPTED
     $mail->Port       = 465;                                     //TCP PORT TO CONNECT TO 465 587
 
     //Recipients
